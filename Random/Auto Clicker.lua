2d9781987a5453079c43c2736f5ba50bb071f7b5
halloween <107208142+halloweevn@users.noreply.github.com>
2022-07-23 10:57:00 +1000

    Update Auto Clicker.lua

--- Random/Auto Clicker.lua
+++ Random/Auto Clicker.lua
@@ -1,7 +1,17 @@
---// Toggle {true/false}
-getgenv().AutoClicker = true
+--// Toggle Q
+AutoClicker = false
+local Mouse = game.Players.LocalPlayer:GetMouse()
 
-while getgenv().AutoClicker == true do
-    wait()
-    mouse1click()
-end
+Mouse.KeyDown:connect(function(Key)
+    if Key == "q" and AutoClicker == false then
+        AutoClicker = true
+        getgenv().AutoClicker = true
+        while getgenv().AutoClicker == true do
+            wait()
+            mouse1click()
+        end
+        elseif Key == "q" and AutoClicker == true then
+            AutoClicker = false
+            getgenv().AutoClicker = false
+    end
+end)
