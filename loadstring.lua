2a189c9d9eadd5434253916dd3b8344ec89609f2
halloween <107208142+halloweevn@users.noreply.github.com>
2022-07-25 15:45:02 +1000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -6,6 +6,8 @@ elseif game.PlaceId == 9183932460 then -- Untitled Hood
         Text = "Script Coming Soon!";
         Duration = 7;
     })
+elseif game.PlaceId == 8884334497 then -- Mining Clicker Simulator
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Clicker%20Simulator/src'))()
 else
     game.StarterGui:SetCore("SendNotification",{
         Title = "Error..";
