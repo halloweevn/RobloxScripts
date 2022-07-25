ad4db75157281265105f7924f9688d9d143d299b
halloween <107208142+halloweevn@users.noreply.github.com>
2022-07-25 15:50:47 +1000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,6 +1,3 @@
-getgenv().Key = getgenv().Key
-getgenv().Color = getgenv().Color
-
 if game.PlaceId == 9551640993 then -- Mining Simulator 2
     loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Simulator%202/src'))()
 elseif game.PlaceId == 9183932460 then -- Untitled Hood
@@ -10,7 +7,7 @@ elseif game.PlaceId == 9183932460 then -- Untitled Hood
         Duration = 7;
     })
 elseif game.PlaceId == 8884334497 then -- Mining Clicker Simulator
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Clicker%20Simulator/src'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Clicker%20Simulator/src.lua'))()
 else
     game.StarterGui:SetCore("SendNotification",{
         Title = "Error..";
