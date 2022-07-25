f4962309afaa7970afab018a677b43b477ee4b6e
halloween <107208142+halloweevn@users.noreply.github.com>
2022-07-25 15:46:59 +1000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,3 +1,6 @@
+getgenv().Key = getgenv().Key
+getgenv().Color = getgenv().Color
+
 if game.PlaceId == 9551640993 then -- Mining Simulator 2
     loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Simulator%202/src'))()
 elseif game.PlaceId == 9183932460 then -- Untitled Hood
