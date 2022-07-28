b3d68193757463d9a46796f9ab187d8550eaeaf7
halloween <107208142+halloweevn@users.noreply.github.com>
2022-07-28 23:56:39 +1000

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,15 +1,15 @@
 if game.PlaceId == 9551640993 then -- Mining Simulator 2
     loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Simulator%202/src'))()
+elseif game.PlaceId == 8884334497 then -- Mining Clicker Simulator
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Clicker%20Simulator/src.lua'))()
+elseif game.PlaceId == 6677985923 then -- Millionaire Empire Tycoon
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Millionaire%20Empire%20Tycoon/src.lua'))()
 elseif game.PlaceId == 9183932460 then -- Untitled Hood
     game.StarterGui:SetCore("SendNotification",{
         Title = "Soon..";
         Text = "Script Coming Soon!";
         Duration = 7;
     })
-elseif game.PlaceId == 8884334497 then -- Mining Clicker Simulator
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Mining%20Clicker%20Simulator/src.lua'))()
-elseif game.PlaceId == 6677985923 then -- Millionaire Empire Tycoon
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/RobloxScripts/main/Millionaire%20Empire%20Tycoon/src.lua'))()
 else
     game.StarterGui:SetCore("SendNotification",{
         Title = "Error..";
